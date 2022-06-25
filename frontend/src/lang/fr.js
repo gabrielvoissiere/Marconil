f3c5508870f7c57c866d42f387b240c51a763633
Gabriel Voissiere <gabvoissiere.pro@yahoo.com>
2022-06-25 14:10:40 +0200

    Je veux que le text de la page home soit disponible en anglais et en francais

--- frontend/src/lang/fr.js
+++ frontend/src/lang/fr.js
@@ -1,10 +1,59 @@
 const fr = {
     title: {
-        main_title: 'Marconil'
+        main_title: "Marconil"
+    },
+    header: {},
+    footer: {
+        title: "Marconil -<br>web design + développement web",
+        contact: "Contact"
     },
     home_page: {
-        title: 'is a web design and web/mobile \n development agency based in the south of France '
+        above_the_fold: {
+            title: "est une agence de conception web et de développement web/mobile <br> basée dans le sud de la France ",
+            btn: "demander un devis"
+        },
+        work: {
+            title: "Comment travaillons-nous ?",
+            subtitle: "Notre agence prend le temps avec vous de comprendre exactement et précisément ce que vous voulez pour votre usine.",
+            first_block: {
+                title: "Compréhension",
+                text: "Nous commencerons par un appel à mieux nous connaître. Connaissez-vous, faites partie du travail pour comprendre précisément ce que vous voulez. Nous vous donnerons quelques conseils, et lorsque vous nous donnerez le feu vert, nous y allons !"
+            },
+            second_block: {
+                title: "Stratégie",
+                text: "Nous commencerons par un appel à mieux nous connaître. Connaissez-vous, faites partie du travail pour comprendre précisément ce que vous voulez. Nous vous donnerons quelques conseils, et lorsque vous nous donnerez le feu vert, nous y allons !"
+            },
+            third_block: {
+                title: "Concevoir",
+                text: "Nous commencerons par un appel à mieux nous connaître. Connaissez-vous, faites partie du travail pour comprendre précisément ce que vous voulez. Nous vous donnerons quelques conseils, et lorsque vous nous donnerez le feu vert, nous y allons !"
+            },
+            fourth_block: {
+                title: "Développer",
+                text: "Nous commencerons par un appel à mieux nous connaître. Connaissez-vous, faites partie du travail pour comprendre précisément ce que vous voulez. Nous vous donnerons quelques conseils, et lorsque vous nous donnerez le feu vert, nous y allons !"
+            }
+        },
+        who: {
+            title: "Avec qui?",
+            subtitle: "Portfolio, design site web, e-commerce, profession libérale, usine, branding... "
+        },
+        projects: {
+            title: "Quelques-uns de nos projets"
+        },
+        value: {
+            title: "Nous crééons de la valeur"
+        },
+        contact: {
+            title: "Contactez nous",
+            subtitle: "Nous répondons en 4 heures"
+        },
+        sentence: {
+            first: "Soyez prudent,",
+            second: "seul les personnes avec de l'ambitions peuvent travailler avec nous."
+        }
     }
 }
 
-export { fr }
+
+export {
+    fr
+}
